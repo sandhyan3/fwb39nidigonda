017a8aff5e3c9a5fdec7e073817f5a6151f3536a
sandhyan3 <S545239@NWMISSOURI.EDU>
2021-11-03 15:14:20 -0500

    added balls files

--- routes/balls.js
+++ routes/balls.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('ball', { title: 'Search Results for ball' });
+  res.render('balls', { title: 'Search Results for balls' });
 });
 
 module.exports = router;
